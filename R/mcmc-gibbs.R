824081fef767b6c23783d5ef37f18f1a669a9f09
rjbgoudie <rjbgoudie@users.noreply.github.com>
2011-06-07 08:47:11 +0100

    Expose the logScore tracking, by adding it as a statistic

--- R/mcmc-gibbs.R
+++ R/mcmc-gibbs.R
@@ -498,6 +498,16 @@ BNGibbsSampler <- function(data,
   etBinsSize <- 1000
   etbins <- matrix(ncol = numberOfNodes^2, nrow = etBinsIncrement)
   nBurnin <- 0
+
+  statistics <- lapply(statistics, function(f){
+    function(currentNetwork){
+      f(currentNetwork[[1]])
+    }
+  })
+  defaultStatistics <- list(score = function(currentNetwork){
+    sum(currentNetwork[[5]])
+  })
+  statistics <- c(statistics, defaultStatistics)
   nStatistics <- length(statistics)
   statisticsTable <- matrix(ncol = nStatistics,
                             nrow = etBinsSize * etBinsIncrement)
@@ -575,7 +585,7 @@ BNGibbsSampler <- function(data,
     if (nSteps > nBurnin){
       step <- nSteps - nBurnin
       for (i in seq_along(statistics)){
-        statisticsTable[step, i] <<- statistics[[i]](currentNetwork[[1]])
+        statisticsTable[step, i] <<- statistics[[i]](currentNetwork)
       }
     }
   }
